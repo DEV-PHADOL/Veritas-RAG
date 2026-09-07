a2595b6409da7fd752b5d0aac13ca9f014624797
DEV-PHADOL <devphadol1919@gmail.com>
2026-09-07 14:27:55 +0530

    connection

---
+++ db/connection.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("SUPABASE_DB_URL")
+
+if not DATABASE_URL:
+    raise ValueError("SUPABASE_DB_URL is not set.")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
